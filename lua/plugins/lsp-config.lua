e6b4643fe8641b8b2a90a3c17acb05e8265b10ef
surapong.n <surapong.n@nida.ac.th>
2024-01-03 08:54:09 +0700

    update

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,74 +1,75 @@
 local servers = {
-  "tailwindcss",
-  "tsserver",
-  "gopls",
-  "pyright",
-  "emmet_ls",
-  "html",
-  "lua_ls",
-  "yamlls",
-  "denols",
-  "dockerls",
-  "bashls",
-  "jsonls",
-  "cssls",
+	"tailwindcss",
+	"tsserver",
+	"gopls",
+	"pyright",
+	"emmet_ls",
+	"html",
+	"lua_ls",
+	"yamlls",
+	"denols",
+	"dockerls",
+	"bashls",
+	"jsonls",
+	"cssls",
 }
 
 local linter_and_fotmatter = {
-  "autopep8",
-  "djlint",
-  "prettier",
-  "stylua",
-  "gofumpt",
-  "golines",
-  "goimports-reviser",
+	"autopep8",
+	"djlint",
+	"eslint_d",
+	"prettier",
+	"stylua",
+	"gofumpt",
+	"golines",
+	"goimports-reviser",
 }
 
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
-    config = function()
-      require("mason").setup()
-      -- custom cmd to install linter and formatter
-      vim.api.nvim_create_user_command("MasonInstallAll", function()
-        if linter_and_fotmatter and #linter_and_fotmatter > 0 then
-          vim.cmd("MasonInstall " .. table.concat(linter_and_fotmatter, " "))
-        end
-      end, {})
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = servers,
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
+		config = function()
+			require("mason").setup()
+			-- custom cmd to install linter and formatter
+			vim.api.nvim_create_user_command("MasonInstallAll", function()
+				if linter_and_fotmatter and #linter_and_fotmatter > 0 then
+					vim.cmd("MasonInstall " .. table.concat(linter_and_fotmatter, " "))
+				end
+			end, {})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = servers,
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require("lspconfig")
+			local lspconfig = require("lspconfig")
 
-      for _, lsp in ipairs(servers) do
-        lspconfig[lsp].setup({
-          capabilities = capabilities,
-        })
-      end
+			for _, lsp in ipairs(servers) do
+				lspconfig[lsp].setup({
+					capabilities = capabilities,
+				})
+			end
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
