e36234a7251de747a8ee15401ce5f80e869d3282
surapong.n <surapong.n@nida.ac.th>
2024-01-03 09:41:13 +0700

    add gopher

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -72,4 +72,14 @@ return {
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
 	},
+	{
+		"olexsmir/gopher.nvim",
+		ft = "go",
+		config = function(_, opts)
+			require("gopher").setup(opts)
+		end,
+		build = function()
+			vim.cmd([[silent! GoInstallDeps]])
+		end,
+	},
 }
