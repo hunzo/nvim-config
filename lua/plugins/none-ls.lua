7d1c08756080fb72491330d7d99a6e2a41a887e7
surapong.n <surapong.n@nida.ac.th>
2024-01-03 08:21:21 +0700

    update config prettier

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -32,7 +32,13 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.prettier.with({
+          extra_args = {
+            "--no-semi",
+            "--single-qoute",
+            "--jsx-single-qoute",
+          },
+        }),
         -- null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.diagnostics.djlint,
         null_ls.builtins.formatting.gofumpt,
--- readme.md
+++ readme.md
@@ -1,4 +1,15 @@
 # my lazy.vim config
+
 ```
 git clone https://github.com/hunzo/nvim-config.git ~/.config/nvim && nvim
 ```
+
+# Mason install linter and formatter
+
+- autopep8
+- djlint
+- gofumpt
+- goimports-reviser
+- golines
+- prettier
+- stylua
